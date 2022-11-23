35421ad78380ca375697f797d539b1f4758f1ee6
Ntsakelo <Ntsakeloarnold00@gmail.com>
2022-11-23 13:42:55 +0200

    added spaza registration functionality and suggestions display

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
-import express from 'express';
-import handlebars from 'express-handlebars';
-import bodyParser from 'body-parser';
-import session from 'express-session';
-import flash from 'express-flash';
-import pgPromise from 'pg-promise';
-import SpazaSuggest from './spaza-suggest.js';
-import SpazaRoutes from './routes/spaza-routes.js';
-
+import express from "express";
+import handlebars from "express-handlebars";
+import bodyParser from "body-parser";
+import session from "express-session";
+import flash from "express-flash";
+import pgPromise from "pg-promise";
+import SpazaSuggest from "./spaza-suggest.js";
+import SpazaRoutes from "./routes/spaza-routes.js";
 
 const pgp = pgPromise();
 
@@ -47,14 +46,18 @@ app.use(express.static("public"));
 //factory functions
 const spazaSuggest = SpazaSuggest(db);
 const spazaRoutes = SpazaRoutes(spazaSuggest);
-app.get('/',spazaRoutes.showLogin)
-app.get('/register',spazaRoutes.showRegister);
-app.get('/suggest',spazaRoutes.showSuggest);
-app.post('/clientRegister',spazaRoutes.registerClient);
-app.post('/clientLogin',spazaRoutes.loginClient)
-app.post('/suggest',spazaRoutes.suggest)
+app.get("/", spazaRoutes.showLogin);
+app.get("/register", spazaRoutes.showRegister);
+app.get("/suggest", spazaRoutes.showSuggest);
+app.post("/clientRegister", spazaRoutes.registerClient);
+app.post("/clientLogin", spazaRoutes.loginClient);
+app.post("/suggest", spazaRoutes.suggest);
+app.get("/spazaRegister", spazaRoutes.showShopRegister);
+app.post("/spaza", spazaRoutes.registerShop);
+app.get("/areaSuggestions", spazaRoutes.showAreaSuggestions);
+// app.get("/spaza/accept/:id", spazaRoutes.accept);
 let port = process.env.PORT || 4002;
 
-app.listen(port, function(){
-  console.log('app started at port :',port)
-})
+app.listen(port, function () {
+  console.log("app started at port :", port);
+});
--- routes/spaza-routes.js
+++ routes/spaza-routes.js
@@ -1,80 +1,121 @@
+export default function spazaRoutes(spazaSuggest) {
+  async function showLogin(req, res, next) {
+    try {
+      res.render("index");
+    } catch (err) {
+      next(err);
+    }
+  }
+  async function showRegister(req, res, next) {
+    try {
+      res.render("register");
+    } catch (err) {
+      next(err);
+    }
+  }
+  async function showSuggest(req, res, next) {
+    try {
+      const clientId = req.session.user.id;
 
-
-export default function spazaRoutes(spazaSuggest){
-    async function showLogin(req,res,next){
-        try{
-           res.render('index')
-        }catch(err){
-            next(err)
+      res.render("suggest", {
+        places: await spazaSuggest.areas(),
+        suggestions: await spazaSuggest.suggestions(clientId),
+      });
+    } catch (err) {
+      next(err);
+    }
+  }
+  async function registerClient(req, res, next) {
+    try {
+      let username = req.body.clientname;
+      if (!username) {
+        req.flash("register", "Please enter your name");
+        return res.redirect("/register");
+      }
+      let code = await spazaSuggest.registerClient(username);
+      req.flash("register", `Successful,Login using the code : ${code} `);
+      res.redirect("/register");
+    } catch (err) {
+      next(err);
+    }
+  }
+  async function loginClient(req, res, next) {
+    try {
+      let code = req.body.clientCode;
+      let results = await spazaSuggest.clientLogin(code);
+      if (!results) {
+        results = await spazaSuggest.spazaLogin(code);
+        if (!results) {
+          res.redirect("/");
+        } else {
+          req.session.spaza = results;
+          res.redirect("/areaSuggestions");
         }
+      } else {
+        req.session.user = results;
+        res.redirect("/suggest");
+      }
+    } catch (err) {
+      next(err);
     }
-    async function showRegister(req,res,next){
-        try{
-          res.render('register')
-        }catch(err){
-            next(err)
-        }
+  }
+  async function suggest(req, res, next) {
+    try {
+      let name = req.body.places;
+      let suggestion = req.body.productname;
+      let clientId = req.session.user.id;
+      let results = await spazaSuggest.findAreaByName(name);
+      console.log(results);
+      await spazaSuggest.suggestProduct(results.id, clientId, suggestion);
+      res.redirect("/suggest");
+    } catch (err) {
+      next(err);
     }
-    async function showSuggest(req,res,next){
-        try{
-           const clientId = req.session.user.id
-           
-        res.render('suggest',{
-            places: await spazaSuggest.areas(),
-            suggestions: await spazaSuggest.suggestions(clientId)
-        })
-        }catch(err){
-            next(err)
-        }
+  }
+  async function showShopRegister(req, res, next) {
+    try {
+      res.render("spaza", {
+        areas: await spazaSuggest.areas(),
+      });
+    } catch (err) {
+      next(err);
     }
-    async function registerClient(req,res,next){
-        try{
-          let username = req.body.clientname;
-          if(!username){
-            req.flash('register','Please enter your name')
-            return res.redirect('/register');
-          }
-         let code = await spazaSuggest.registerClient(username);
-          req.flash('register',`Successful,Login using the code : ${code} `)
-          res.redirect('/register')
-        }catch(err){
-            next(err)
-        }
+  }
+  async function registerShop(req, res, next) {
+    try {
+      let name = req.body.shopname;
+      let area = req.body.shoparea;
+      if (!name || !area) {
+        return res.redirect("/spazaRegister");
+      } else {
+        let results = await spazaSuggest.findAreaByName(area);
+        let code = await spazaSuggest.registerSpaza(name, results.id);
+        req.flash("registerShop", `Successful,Login using the code : ${code}`);
+        res.redirect("/spazaRegister");
+      }
+    } catch (err) {
+      next(err);
     }
-    async function loginClient(req,res,next){
-        try{
-          let code = req.body.clientCode
-          let results = await spazaSuggest.clientLogin(code)
-          if(!results){
-            res.redirect('/')
-          }else{
-            req.session.user = results
-            res.redirect('/suggest')
-          }
-        }catch(err){
-            next(err)
-        }
+  }
+  async function showAreaSuggestions(req, res, next) {
+    try {
+      let areaId = req.session.spaza.area_id;
+      res.render("areaSuggestions", {
+        suggestions: await spazaSuggest.suggestionsForArea(areaId),
+      });
+    } catch (err) {
+      next(err);
     }
-    async function suggest(req,res,next){
-        try{
-          let name = req.body.places;
-          let suggestion = req.body.productname;
-          let clientId = req.session.user.id;
-          let results = await spazaSuggest.findAreaByName(name);
-              console.log(results) 
-              await spazaSuggest.suggestProduct(results.id, clientId, suggestion);
-              res.redirect('/suggest')
-
-        }catch(err){
-            next(err)
-        }
-    }
-    return {
-        showLogin,
-        showRegister,
-        showSuggest,
-        registerClient,
-        loginClient,
-        suggest
-    }
+  }
+  return {
+    showLogin,
+    showRegister,
+    showSuggest,
+    registerClient,
+    loginClient,
+    suggest,
+    showShopRegister,
+    registerShop,
+    showAreaSuggestions,
+  };
 }
